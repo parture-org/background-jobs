6ae02e08cbd373805c43e76d05fc5ad6489cff69
asonix <asonix@asonix.dog>
2018-11-17 16:59:16 -0600

    Delete actix and tokio examples

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,5 @@ pub use background_jobs_core::{
     Backoff, JobError, JobInfo, JobStatus, MaxRetries, Processor, Processors, ShouldStop, Storage,
 };
 
-#[cfg(feature = "background-jobs-tokio")]
-pub use background_jobs_tokio::{JobRunner, ProcessorHandle};
-
-#[cfg(feature = "background-jobs-actix")]
-pub use background_jobs_actix::{JobsActor, JobsBuilder, QueueJob};
-
-#[cfg(feature = "background-jobs-server-tokio")]
-pub use background_jobs_server_tokio::{ServerConfig, SpawnerConfig, WorkerConfig};
+#[cfg(feature = "background-jobs-server")]
+pub use background_jobs_server::{ServerConfig, SpawnerConfig, WorkerConfig};
